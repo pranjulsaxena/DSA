c97dda2c0de4bbbf0ebd5219a629943eb903e221
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-07-04 22:05:09 +0530

    Tower of hanoi

--- recursion/Tower_of_Hanoi.java
+++ recursion/Tower_of_Hanoi.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Tower_of_Hanoi {
+    static void Solve(char source, char helper, char destination, int n) {
+        if (n == 1) {
+            System.out.println(n + " moves from " + source + "to " + destination);
+            return;
+        }
+        Solve(source, destination, helper, n - 1);
+        System.out.println(n + " moves from " + source + "to " + destination);
+        Solve(helper, source, destination, n - 1);
+        return;
+
+    }
+
+    public static void main(String[] args) {
+        Solve('A', 'B', 'C', 3);
+    }
 }
