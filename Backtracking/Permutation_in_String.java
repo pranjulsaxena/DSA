eb0bc823312df12b56598affe40962089616f827
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-08-04 14:40:50 +0530

    recursion is controlled for unique permutations of string in output

--- Backtracking/Permutation_in_String.java
+++ Backtracking/Permutation_in_String.java
@@ -1,22 +1,53 @@
 package Backtracking;
 
+import java.util.ArrayList;
+
 public class Permutation_in_String {
     static void solve(String str){
         if(str.isEmpty()){
             return;
         }
-        helper(str,0,"",str.length()-1);
+        helper(str,"");
     }
-    static void helper(String str,int index, String curr, int i){
+    static void helper(String str, String curr){
         if(str.isEmpty()){
             System.out.println(curr);
             return;
         }
         for(int j = 0; j<str.length(); j++){
-            helper(str.substring(0,j)+str.substring(j+1),index,curr+str.charAt(j),0);
+                helper(str.substring(0,j)+str.substring(j+1),curr+str.charAt(j));
+
         }
     }
     public static void main(String[] args) {
-        solve("AbC");
+        solve("ABA");
+    }
+}
+
+// controller recursion used in avoid duplicated permutation of string for the duplicate character in input string
+
+public class Permutation_in_String {
+    static void solve(String str){
+        if(str.isEmpty()){
+            return;
+        }
+        helper(str,"");
+    }
+    static void helper(String str, String curr){
+        if(str.isEmpty()){
+            System.out.println(curr);
+            return;
+        }
+        ArrayList<Character> char_list = new ArrayList<>();
+        for(int j = 0; j<str.length(); j++){
+            // controller recursion
+            if(!char_list.contains(str.charAt(j))){
+                char_list.add(str.charAt(j));
+                helper(str.substring(0,j)+str.substring(j+1),curr+str.charAt(j));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        solve("ABA");
     }
 }
