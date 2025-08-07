3f3772f1075bfac862ea16bb98a0efd5ea87cb2e
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-08-07 23:54:32 +0530

    Optimised Solution

--- Backtracking/Largest_in_K_swap.java
+++ Backtracking/Largest_in_K_swap.java
@@ -1,63 +1,119 @@
 package Backtracking;
-import java.util.ArrayList;
 
+// gfg
 
+//public class Largest_in_K_swap {
+//    static String findMaximumNum(StringBuilder str,int k){
+//        if(str.isEmpty()){
+//            return "";
+//        }
+//        return helper(str,0,k);
+//    }
+//    static String helper(StringBuilder str,int index,int k){
+//        if(index == k){
+//            return str.toString();
+//        }
+//
+//            ArrayList<Integer> maxx = max(str,index);
+//           int ch = Character.getNumericValue(str.charAt(index));
+//            int maxxValue ;
+//            boolean a = false;
+//
+//
+//                for (Integer integer : maxx) {
+//                    maxxValue = Character.getNumericValue(str.charAt(integer));
+//                    if (maxxValue > ch) {
+//                        a= true;
+//                        swap(str, index, integer);
+//                        helper(str, index + 1, k);
+//
+//
+//                    }else{
+//                        helper(str,index+1,k);
+//                    }
+//                }
+//                if(!a){
+//                    helper(str,index+1,k);
+//                }
+//        return str.toString();
+//    }
+//    static ArrayList<Integer> max(StringBuilder str, int index){
+//        int []array = new int[str.substring(index+1,str.length()).length()];
+//
+//        StringBuilder newStr = new StringBuilder(str.substring(index+1,str.length()));
+//
+//        for(int j = 0; j<newStr.length(); j++){
+//            array[j] = Character.getNumericValue(newStr.charAt(j));
+//        }
+//        int maxx = Integer.MIN_VALUE;
+//        for(int j = 0 ; j<array.length; j++){
+//            if(array[j]>maxx){
+//                maxx=array[j];
+//            }
+//
+//        }
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        for(int i =index+1 ; i<str.length(); i++){
+//            if(Character.getNumericValue(str.charAt(i))==maxx){
+//                list.add(i);
+//            }
+//        }
+//        return list;
+//
+//    }
+//    static void swap(StringBuilder str,int index,int i){
+//        char temp = str.charAt(index);
+//        str.setCharAt(index,str.charAt(i));
+//        str.setCharAt(i,temp);
+//    }
+//
+//    public static void main(String[] args) {
+//        StringBuilder str = new StringBuilder("42431636");
+//        System.out.println(findMaximumNum(str,3));
+//    }
+//
+//}
 
 public class Largest_in_K_swap {
-    static String findMaximumNum(StringBuilder str,int k){
+    static String findMaximumNum(String str, int k){
         if(str.isEmpty()){
             return "";
         }
-        return helper(str,0,1);
+        StringBuilder res = new StringBuilder(str);
+        StringBuilder strr = new StringBuilder(str);
+        helper(strr,0,k,res);
+        return res.toString();
+
     }
-    static String helper(StringBuilder str,int index,int k){
-        if(index == k){
-            return str.toString();
+    static void helper(StringBuilder str,int index,int k, StringBuilder response){
+        if(index==str.length()||k==0){
+
+            return ;
         }
+        boolean a = false;
+        char max = max(str,index);
+        for(int i = index+1; i<str.length(); i++){
 
-            ArrayList<Integer> maxx = max(str,index);
-           int ch = Character.getNumericValue(str.charAt(index));
-            int maxxValue ;
-            boolean a = false;
+            if(str.charAt(index)<str.charAt(i)&&str.charAt(i)==max){
+                a = true;
+                // swap
+                swap(str,index,i);
 
-
-                for (Integer integer : maxx) {
-                    maxxValue = Character.getNumericValue(str.charAt(integer));
-                    if (maxxValue > ch) {
-                        a= true;
-                        swap(str, index, integer);
-                        helper(str, index + 1, k);
-                        swap(str, index, integer);Integer.parseInt(str.toString());
-                    }
+                if(str.toString().compareTo(response.toString()) > 0){
+                    response.setLength(0);
+                    response.append(str.toString());
                 }
-                if(!a){
-                    helper(str,index+1,k);
-                }
-        return str.toString();
-    }
-    static ArrayList<Integer> max(StringBuilder str, int index){
-        int []array = new int[str.substring(index+1,str.length()).length()];
 
-        StringBuilder newStr = new StringBuilder(str.substring(index+1,str.length()));
-
-        for(int j = 0; j<newStr.length(); j++){
-            array[j] = Character.getNumericValue(newStr.charAt(j));
-        }
-        int maxx = Integer.MIN_VALUE;
-        for(int j = 0 ; j<array.length; j++){
-            if(array[j]>maxx){
-                maxx=array[j];
-            }
-
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for(int i =0 ; i<str.length(); i++){
-            if(Character.getNumericValue(str.charAt(i))==maxx){
-                list.add(i);
+                helper(str,index+1,k-1,response);
+                // backtracking
+                swap(str,index,i);
             }
         }
-        return list;
+        if(!a){
+            helper(str,index+1,k,response);
+        }
+
 
     }
     static void swap(StringBuilder str,int index,int i){
@@ -65,10 +121,19 @@ public class Largest_in_K_swap {
         str.setCharAt(index,str.charAt(i));
         str.setCharAt(i,temp);
     }
-
-    public static void main(String[] args) {
-        StringBuilder str = new StringBuilder("1234");
-        System.out.println(findMaximumNum(str,2));
+    static char max(StringBuilder str,int index){
+        char[] array = str.toString().toCharArray();
+        char max= ' ';
+        for(int i = index+1 ; i<array.length; i++){
+            if(array[i]>max){
+                max=array[i];
+            }
+        }
+        return max;
     }
 
+    public static void main(String[] args) {
+        String demo = "102938475619283746";
+        System.out.println(findMaximumNum(demo,3));
+    }
 }
--- Backtracking/Permutation_in_string_backtracking.java
+++ Backtracking/Permutation_in_string_backtracking.java
@@ -1,4 +1,70 @@
 package Backtracking;
 
+//public class Permutation_in_string_backtracking {
+//    static void permutation(StringBuilder str){
+//        if(str.isEmpty()){
+//            return;
+//        }
+//        helper(str,0);
+//    }
+//    static void helper(StringBuilder str,int index){
+//    if(index == str.length()){
+//        System.out.println(str);
+//        return;
+//    }
+//
+//       for(int j = index; j<str.length(); j++){
+//           char a = str.charAt(index);
+//           char b = str.charAt((index+j)-index);
+//           str.setCharAt(index,b);
+//           str.setCharAt((index+j)-index,a);
+//           helper(str,index+1);
+//
+//       }
+//    }
+//    public static void main(String[] args) {
+//        StringBuilder str = new StringBuilder("abc");
+//        permutation(str);
+//    }
+//}
+
+// there is a big bug in above code.. i.e. no backtract ( rever change) as it is pass by refernce that modification is as
+
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Permutation_in_string_backtracking {
+    static void permutation(StringBuilder str){
+        if(str.isEmpty()){
+            return;
+        }
+        helper(str,0);
+    }
+    static void helper(StringBuilder str,int index){
+        if(index == str.length()){
+            System.out.println(str);
+            return;
+        }
+        ArrayList<Character> list = new ArrayList<>();
+        for(int j = index; j<str.length(); j++){
+            if(!list.contains(str.charAt(j))){
+                list.add(str.charAt(j));
+                // swap
+                swap(str,index,j);
+                helper(str,index+1);
+                //Backtrack (revert changes)
+                swap(str,index,j);
+            }
+        }
+    }
+    static void swap(StringBuilder str, int index, int j){
+        char temp = str.charAt(index);
+        str.setCharAt(index,str.charAt(j));
+        str.setCharAt(j,temp);
+    }
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("aba");
+        permutation(str);
+    }
 }
+
