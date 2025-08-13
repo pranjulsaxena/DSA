8d1984f0f36994805ca0958fa057ebd645076c57
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-08-13 23:48:26 +0530

    Word Break using backtracking -- best approach

--- Backtracking/Word_Break.java
+++ Backtracking/Word_Break.java
@@ -1,31 +1,83 @@
 package Backtracking;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+//public class Word_Break {
+//    static List<String> wordBreak(String s, List<String> wordDict){
+//        List<String> list = new ArrayList<>();
+//        if(s.isEmpty() || wordDict.isEmpty()){
+//            return list;
+//        }
+//        helper(s,0,"",wordDict, list);
+//        return list;
+//    }
+//    static void helper(String str,int index, String currString,List<String> wordDict, List<String> list){
+//        if(index==str.length()){
+//            list.add(currString.trim());
+//            return;
+//        }
+//        for(int i = index; i<str.length(); i++){
+//            String inDict = str.substring(index,i+1);
+//            if(isinDict(wordDict,inDict)){
+////                currString=currString+inDict+",";
+////                helper(str,i+1,currString,dict);
+//                helper(str,i+1,currString+inDict+" ",wordDict,list);
+//            }
+//        }
+//    }
+//    static boolean isinDict(List<String> dict, String inDict){
+//        for(int i =0; i<dict.size(); i++){
+//            if(dict.get(i).equals(inDict)){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    public static void main(String[] args) {
+//        String s = "catsanddog";
+//        List<String> wordDict = new ArrayList<>();
+//        String[] array={"cat","cats","and","sand","dog"};
+//        for(int i=0; i<array.length; i++){
+//            wordDict.add(array[i]);
+//        }
+//        System.out.println( wordBreak(s,wordDict));
+//    }
+//}
+
+
+// Using backtracking ----> preferred approach
+
 
 public class Word_Break {
-    static void Solve(String str,String []dict){
-        if(str.isEmpty() || dict.length==0){
-            return ;
+    static List<String> wordBreak(String s, List<String> wordDict){
+        List<String> list = new ArrayList<>();
+        if(s.isEmpty() || wordDict.isEmpty()){
+            return list;
         }
-        helper(str,0,"",dict);
+        StringBuilder currString = new StringBuilder();
+        helper(s,0,wordDict, list,currString);
+        return list;
     }
-    static void helper(String str,int index, String currString,String[] dict){
+    static void helper(String str,int index,List<String> wordDict, List<String> list,StringBuilder currString){
         if(index==str.length()){
-            String[]answer=currString.split(",");
-            System.out.println(Arrays.toString(answer));
+            list.add(currString.toString().trim());
             return;
         }
         for(int i = index; i<str.length(); i++){
             String inDict = str.substring(index,i+1);
-            if(isinDict(dict,inDict)){
-//                currString=currString+inDict+",";
-                helper(str,i+1,currString+inDict+" ",dict);
+            int length = currString.length();
+            if(isinDict(wordDict,inDict)){
+                currString.append(inDict+" ");
+                helper(str,i+1,wordDict,list,currString);
+                // backtrack
+                currString.setLength(length);
             }
         }
     }
-    static boolean isinDict(String[] dict, String inDict){
-        for(int i =0; i<dict.length; i++){
-            if(dict[i].equals(inDict)){
+    static boolean isinDict(List<String> dict, String inDict){
+        for(int i =0; i<dict.size(); i++){
+            if(dict.get(i).equals(inDict)){
                 return true;
             }
         }
@@ -33,7 +85,18 @@ public class Word_Break {
     }
     public static void main(String[] args) {
         String s = "pineapplepenapple";
-        String []dict ={"apple","pen","applepen","pine","pineapple"};
-        Solve(s,dict);
+        List<String> wordDict = new ArrayList<>();
+        String[] array={"apple","pen","applepen","pine","pineapple"};
+        for(int i=0; i<array.length; i++){
+            wordDict.add(array[i]);
+        }
+        System.out.println( wordBreak(s,wordDict));
     }
 }
+
+
+
+
+
+
+
