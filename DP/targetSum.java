8eaa15f0b2fd1ef763acf2b4882389a72e3c2ba5
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-09-06 23:46:28 +0530

    target sum -- DP

--- DP/targetSum.java
+++ DP/targetSum.java
@@ -1,5 +1,7 @@
 package DP;
 
+import java.util.Arrays;
+
 public class targetSum {
     // using recursion. time complexity- O(2^N);
     static int Solve(int[]arr,int target){
@@ -18,7 +20,41 @@ public class targetSum {
         return include + exclude;
     }
     public static void main(String[] args) {
-        System.out.println(Solve(new int[]{1,1,1,1,1},3));
+        System.out.println(findTargetSumWays(new int[]{1,1,1,1,1},3));
+    }
+
+    // using DP. Time Complexity: O(N*S) , space complexity: O(N*S)
+    // approach: previous problem, count the partition whose difference is equal to given difference.
+    static int findTargetSumWays(int[]arr, int target){
+        int Arrsum=0;
+        for(int i=0; i<arr.length;i++){
+            Arrsum+=arr[i];
+        }
+        if((Arrsum+target)%2!=0||(Arrsum+target)<0)return 0; // subset sum can't be fractional and can't be negative
+        int sum = (Arrsum + target) / 2;
+        int[][]t = new int[arr.length+1][sum+1];
+        return totalSubset_Sum(arr,sum,t);
+    }
+    static int totalSubset_Sum(int []arr,int sum,int [][]t){
+        for(int i=0; i<=arr.length;i++){
+            for(int j=0; j<=sum;j++){
+                if(i==0) {
+                    t[i][j] = 0;
+                } if(j==0){
+                    t[i][j]=1;
+                }
+            }
+        }
+        for (int i = 1; i <= arr.length; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (arr[i - 1] <= j) {
+                    t[i][j] = t[i - 1][j - arr[i - 1]] + t[i - 1][j];
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+        return t[arr.length][sum];
     }
 
 }
