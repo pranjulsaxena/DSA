044cd2b34d2fad71092190535f8149bb9d0409b1
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-09-15 23:45:12 +0530

    Longest common subsequence -- Tabulation method

--- DP/Least_common_subsequence.java
+++ DP/Least_common_subsequence.java
@@ -51,10 +51,38 @@ public class Least_common_subsequence {
 
         return lcsRec(s1, s2, m, n, memo);
     }
+    // using dp top-down approach(Tabulation) method
+    // space and time complexity:  O(m * n) Time and O(m * n) Space
+    static int lcs3(String S1, String S2) {
+        int m = S1.length();
+        int n = S2.length();
+
+        // Initializing a matrix of size (m+1)*(n+1)
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Building dp[m+1][n+1] in bottom-up fashion
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (S1.charAt(i - 1) == S2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j],
+                            dp[i][j - 1]);
+                }
+            }
+        }
+
+        // dp[m][n] contains length of LCS for S1[0..m-1]
+        // and S2[0..n-1]
+        return dp[m][n];
+    }
+
     public static void main(String[] args) {
         String s1 = "AGGTAB";
         String s2 = "GXTXAYB";
 //        System.out.println(lcs(s1, s2));
-        System.out.println(lcs2(s1, s2));
+//        System.out.println(lcs2(s1, s2));
+          System.out.println(lcs3(s1,s2));
     }
 }
