c124655ff135599ad01f17063dbe754ea5b6bd71
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-10-20 23:59:07 +0530

    Memoization

--- DP/Palindromic_Partition_MCM.java
+++ DP/Palindromic_Partition_MCM.java
@@ -1,7 +1,10 @@
 package DP;
 
 
+import java.util.Arrays;
+
 public class Palindromic_Partition_MCM {
+    // recursive MCM
     static int Solve(String str, int i, int j){
         if(i>=j){
             return 0; // zero partition for one character.
@@ -16,6 +19,26 @@ public class Palindromic_Partition_MCM {
         }
         return min;
     }
+    // Memoization
+    static int Memo(String str, int i, int j, int[][]dp){
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(i>=j){
+            return 0;
+        }
+        if(isPalindrome(str,i,j)){
+            return 0; // if whole substring palindrome . no partition
+        }
+        int min = Integer.MAX_VALUE;
+        for(int k=i; k<j; k++){
+
+            int ans = Memo(str, i,k,dp) + Memo(str,k+1,j,dp)+1;
+            min = Math.min(ans, min);
+        }
+        dp[i][j]=min;
+        return dp[i][j];
+    }
     static Boolean isPalindrome(String str, int i, int j){
         if(i>=j){
             return true;
@@ -24,7 +47,11 @@ public class Palindromic_Partition_MCM {
     }
 
     public static void main(String[] args) {
-        String str = "nitin";
-        System.out.println(Solve(str,0,str.length()-1));
+        String str = "geek";
+        int [][]dp = new int[str.length()+1][str.length()+1];
+        for(int[]i :dp){
+            Arrays.fill(i,-1);
+        }
+        System.out.println(Memo(str,0,str.length()-1,dp));
     }
 }
